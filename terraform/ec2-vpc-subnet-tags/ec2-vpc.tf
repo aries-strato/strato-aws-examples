6b133715f3fcb9f98f1715278986967b6ed59c7d
aries-strato <30353494+aries-strato@users.noreply.github.com>
2018-08-17 15:31:26 +0800

    readme

---
+++ terraform/ec2-vpc-subnet-tags/ec2-vpc.tf
@@ -0,0 +1,118 @@
+# Creating a VPC & Networking
+
+
+# VPC creation
+
+resource "aws_vpc" "myapp_vpc" {
+  cidr_block = "192.168.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "Demo VPC"
+  }
+}
+
+
+# DNS resolver, forwarding to Google DNS 
+# NTP server is IP of time.google.com
+
+resource "aws_vpc_dhcp_options" "foo" {
+  domain_name = "test.local"
+  domain_name_servers = ["8.8.8.8", "8.8.4.4"]
+  ntp_servers = ["216.239.35.12"]
+
+  tags {
+    Name = "Demo"
+  }
+}
+
+# Associating DNS resolver with the DHCP Options association which will be attached to the Demo VPC
+
+resource "aws_vpc_dhcp_options_association" "foo" {
+  vpc_id          = "${aws_vpc.myapp_vpc.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.foo.id}"
+}
+
+# Subnet creation 
+
+resource "aws_subnet" "myapp_subnet"{
+    cidr_block = "192.168.10.0/24"
+    vpc_id = "${aws_vpc.myapp_vpc.id}"
+    tags {
+      Name = "Demo subnet"
+    }
+
+    # Makes sure DHCP configuration is absorbed in the subnet - Symphony specific
+    depends_on = ["aws_vpc_dhcp_options_association.foo"]
+}
+
+# Internet gateway 
+
+resource "aws_internet_gateway" "myapp_gw" {
+  vpc_id = "${aws_vpc.myapp_vpc.id}"
+}
+
+# The default route table will allow each subnet to route to the Internet Gateway
+resource "aws_default_route_table" "default" {
+    default_route_table_id = "${aws_vpc.myapp_vpc.default_route_table_id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.myapp_gw.id}"
+    }
+}
+
+
+############ Instance Creation #############################
+
+# Creating an instance
+resource "aws_instance" "myapp_instance" {
+    ami = "${var.ami_my_image}"
+    instance_type = "${var.instance_type}"
+    subnet_id = "${aws_subnet.myapp_subnet.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    count = "${var.instance_number}"
+    key_name = "tf_demo10"
+    tags{
+        Name="my_instance_${count.index}"
+    }
+}
+
+# Elastic IP Allocation
+
+resource "aws_eip" "myapp_instance_eip" {
+  count = "${var.instance_number}"
+  depends_on = ["aws_internet_gateway.myapp_gw"]
+}
+
+# Associating EIP with instances that get deployed
+
+resource "aws_eip_association" "myapp_eip_assoc" {
+  count = "${var.instance_number}"
+  instance_id = "${element(aws_instance.myapp_instance.*.id, count.index)}"
+  allocation_id = "${element(aws_eip.myapp_instance_eip.*.id, count.index)}"
+}
+
+
+################## Security Group ##################
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all traffic"
+  vpc_id      = "${aws_vpc.myapp_vpc.id}"
+
+  ingress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol        = "-1"
+    from_port       = 0
+    to_port         = 0
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
--- terraform/ec2-vpc-subnet-tags/readme.md
+++ terraform/ec2-vpc-subnet-tags/readme.md
@@ -1,2 +1,2 @@
-# EC2 VPC with DNS and NTP forwarding example
+#EC2 VPC with DNS and NTP forwarding example
 In this terraform we will create a VPC with DNS and NTP forwarding specified. Any subnets that get created will forward their DNS and NTP requests to those respective IP addresses. 
