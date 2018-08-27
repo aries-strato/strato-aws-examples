4d23f66f7d2c2cf387679b451cc841aebe673eb7
aries-strato <30353494+aries-strato@users.noreply.github.com>
2018-08-27 11:57:01 +0800

    fixing cloudinit and readme

--- terraform/ec2-expand-lvm-cloudinit-ebs/main.tf
+++ terraform/ec2-expand-lvm-cloudinit-ebs/main.tf
@@ -1,4 +1,23 @@
 # Create instances, and name them according to count
+
+# Defining cloud config template file 
+
+data "template_file" "ebsdeploy"{
+  template = "${file("./cloudconfig.cfg")}"
+}
+
+data "template_cloudinit_config" "ebsdeploy_config" {
+  gzip = false
+  base64_encode = false
+
+  part {
+    filename     = "cloudconfig.cfg"
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.ebsdeploy.rendered}"
+  }
+}
+
+
 resource "aws_instance" "ec2_instance" {
     ami = "${var.ami_image}"
 
@@ -9,9 +28,11 @@ resource "aws_instance" "ec2_instance" {
     instance_type = "${var.instance_type}"
     count="${var.instance_number}"
 
+    user_data = "${data.template_cloudinit_config.ebsdeploy_config.rendered}"
+
     root_block_device { 
-        volume_size = 100
-        volume_type = "sc1"
+        # Enter larger volume size here in GB, must be larger then images base size
+        volume_size = 250
     }
 
 }
