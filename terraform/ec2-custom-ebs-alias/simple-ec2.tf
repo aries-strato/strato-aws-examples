4cfd4f09a9461493612afa9970549fc010d154d8
aries-strato <30353494+aries-strato@users.noreply.github.com>
2018-08-28 15:38:37 +0800

    Terraform: adding ebs alias example

---
+++ terraform/ec2-custom-ebs-alias/simple-ec2.tf
@@ -0,0 +1,19 @@
+# Create instances, and name them according to count
+resource "aws_instance" "ec2_instance" {
+    ami = "${var.ami_image}"
+
+    tags{
+        Name="instance${count.index}"
+    }
+    #Demo key for testing
+    #key_name = "demo10_aries5"
+    
+    instance_type = "${var.instance_type}"
+    count="${var.instance_number}"
+
+    root_block_device { 
+        volume_size = 100
+        volume_type = "sc1"
+    }
+
+}
---
+++ terraform/ec2-custom-ebs-alias/terraform.tfvars
@@ -0,0 +1,16 @@
+# .tfvars Sample File
+
+# Region Credentials 
+
+symphony_ip = ""
+access_key = ""
+secret_key = ""
+
+# Image AMI ID 
+
+ami_image = ""
+
+# optional
+# instance_type = "<instance-type>"
+# instance_number = <number of instances>
+
